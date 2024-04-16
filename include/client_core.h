6129332702fbac8a837b81ef4e8a0084c1e0a370
Zihang-He <z6he@ucsd.edu>
2024-04-16 16:39:31 -0700

    added client core

---
+++ include/client_core.h
@@ -0,0 +1,38 @@
+#ifndef CLIENT_CORE_H
+#define CLIENT_CORE_H
+
+#include <vector>
+#include <string>
+#include <Winsock2.h>
+
+#include "../networking/server.h"
+#include "../networking/client.h"
+#include "../networking/windows_socket.h"
+
+struct ServerData {
+    std::vector<std::string> messages;  // Received updates from server
+};
+
+class ClientCore {
+public:
+    ClientCore();
+    ~ClientCore();
+
+    void initialize();             // Initialize client resources
+    void shutdown();               // Clean up resources
+    void run();                    // Main client loop
+
+
+    void send_input();                              // Send player input (event?) to the server
+    void receive_updates();                         // Receive updates from server
+    void process_server_data();                     // Process the received data
+    void update_local_game_state();                 // Update local game state based on server updates
+    void render();                                  // Render the game state to the user
+
+    bool connected;                // Connection state
+    Client client;                 // client.conn_sock = socket, 
+    ServerData server_updates;
+};
+
+
+#endif
