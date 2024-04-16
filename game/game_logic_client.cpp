dbe9670954a9d36a4cdecfbbd0c627ac78a505a4
Zihang-He <z6he@ucsd.edu>
2024-04-16 00:45:03 -0700

    added WSAStartup to client too

--- game/game_logic_client.cpp
+++ game/game_logic_client.cpp
@@ -7,9 +7,10 @@
 int main() {
 
     Client client = Client(); // Client connects to localhost by default
-
+    printf("initialized client\n");
+    Sleep(1000);
     const char* str = "Hello, world";
     client.sock_send(strlen(str) + 1, str);
-    
+
     return 0;
 }
