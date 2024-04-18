013c15ced0e0c43c8f889381460aee55d602527c
zfxd <zhang_weiyao@yahoo.com.sg>
2024-04-18 10:28:16 -0700

    graphics in game_logic_client

--- game/game_logic_client.cpp
+++ game/game_logic_client.cpp
@@ -1,6 +1,8 @@
 #include "../include/server.h"
 #include "../include/client.h"
 #include "../include/server_core.h"
+#include "../include/core.h"
+#include "../include/graphics.h"
 #include <windows.h>
 #include <iostream>
 #include <string>
@@ -9,12 +11,23 @@ int main() {
     Client client = Client(); // Client connects to localhost by default
     printf("Client initialized\n");
 
+    // Graphics
+    GLFWwindow* window = Graphics::set_up_window();
+
     std::string input;
     while (true) {
         std::cout << "Enter message to send (q to quit): ";
-        std::getline(std::cin, input);
+        //std::getline(std::cin, input);
+
+        // Graphics callbacks
+        // Render
+        Window::display_callback(window);
+        // Updates
+        Window::idle_callback();
         
-        if (input == "q") {
+        while (!glfwWindowShouldClose) {
+            Window::clean_up();
+            glfwTerminate();
             break; // Exit the loop if the user types 'q'
         }
 
--- include/graphics.h
+++ include/graphics.h
@@ -4,7 +4,8 @@
  * Graphics.h
  * Includes for the basic graphics client
  * Change OpenGL settings here
- * I don't think any other functions will end up here
+ * 
+ * Set up window here!
  */
 
 #include "shader.h"
@@ -12,19 +13,8 @@
 
 class Graphics {
 public:
-	static void setup_opengl_settings() {
+	static void setup_opengl_settings();
 
-		// TODO: Understand this :sob:
-		// Enable Depth Testing
-		glEnable(GL_DEPTH_TEST);
-		// Related to shaders and z value comparisons for the depth buffer.
-		glDepthFunc(GL_LEQUAL);
-
-		// Polygon drawing mode
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-		// Set clear color
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-	}
+	// TODO: Maybe singleton? idk
+	static GLFWwindow* set_up_window();
 };
