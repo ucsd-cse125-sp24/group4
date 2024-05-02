cafde446b3cf6cff7c813d93f178201ca0c94f14
suprithk <suprith.krishnakumar@gmail.com>
2024-05-02 10:48:12 -0700

    init commit

---
+++ physics/game_object.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "graphics.h"
+#include "colliders.h"
+
+
+struct GameObject {
+
+    // have a variable for type player 
+    glm::vec3 position;    // Position of the object in 3D space
+    glm::vec3 velocity;    // Velocity of the object
+    glm::vec3 force; // Acceleration of the object
+    float mass;            // Mass of the object
+    Collider* collider; // bounding volume/box
+
+    // Constructor to initialize properties
+    GameObject(glm::vec3 pos, glm::vec3 vel, glm::vec3 acc, float m, Collider* c);
+};
