194e5332b58d40a1a880a121dee604c4affd5942
zfxd <zhang_weiyao@yahoo.com.sg>
2024-05-30 10:11:48 -0700

    new floor 2 model that spawns the user inside instead

--- graphics/window.cpp
+++ graphics/window.cpp
@@ -125,8 +125,8 @@ void Window::setup_scene() {
 
 	// Floor 6_empty works without rotations
 
-	//Model* mp = new Model("art/models/environment/floor6_empty.fbx");
 	Model* mp = new Model("art/models/chair.fbx");
+	//Model* mp = new Model("art/models/environment/floor2.fbx");
 	mp->set_color(glm::vec3(0.5, 0.5, 0.5));
 	mp->set_world(glm::mat4(1.0f));
 	map = mp;
