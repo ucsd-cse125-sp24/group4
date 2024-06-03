8c3d086555ef04604d5b353e81c4e8d48e4f5e70
zfxd <zhang_weiyao@yahoo.com.sg>
2024-06-02 17:24:57 -0700

    Remove aiPreprocessing step

--- graphics/model.cpp
+++ graphics/model.cpp
@@ -84,7 +84,7 @@ void Model::load_model(const std::string &path)
 
     Assimp::Importer importer;
 
-    const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenBoundingBoxes);
+    const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
     if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
     {
